479c16f71f814204a0f25420a5a55a6ae99d2e86
HCutrone <hcutrone@calpoly.edu>
2022-03-23 02:34:22 -0400

    added simple gear bag and ability to drag devices into bag

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import useGetDevices from './useGetDevices'
 import Device from './components/Device.js'
 import './style.css'
 
-const flexBox = {
+// using a flexbox to display the devices in rows that automatically wrap
+const devicesFlex = {
   display: "flex",
   flexDirection: "row",
   flexWrap: "wrap",
@@ -11,11 +12,32 @@ const flexBox = {
   padding: 30
 }
 
+// using a flexbox to display the devices in rows that automatically wrap
+const gearBagFlex = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "no-wrap",
+  justifyContent: "left",
+  alignItems: "center",
+  overflowX: 'auto',
+  paddingLeft: 20
+}
+
+// gearBagStyle is the CSS to style the div holding the images and text for saved devices
+const gearBagStyle = {
+  width: '100vw',
+  height: 200,
+  background: 'rgb(0, 112, 255, 0.5)',
+  position: 'fixed',
+  bottom: 0,
+}
+
 function App() {
   // offset is the index of the next device to be grabbed from the API
   // width is the number of devices we grab each time we call the API
   const [offset, setOffset] = useState(0)
-  const [width, setWidth] = useState(5)
+  const [width, setWidth] = useState(20)
+  const [gearBag, setGearBag] = useState([])
   const {
     devices,
     hasMore,
@@ -24,7 +46,7 @@ function App() {
   } = useGetDevices(offset, width)
 
   // observer and IntersectionObservers look for the last loaded device on the page
-  //    and change the offset (if there are more devices to load)
+  // and change the offset (if there are more devices to load)
   const observer = useRef()
   const lastDeviceElementRef = useCallback(node => {
     if (loading) {
@@ -46,19 +68,68 @@ function App() {
     }
   }, [loading, hasMore])
 
-  return (
-    <div style={flexBox}>
-      {devices.map((device, index) => {
-        if (devices.length === index + 1) {
-          return <div ref={lastDeviceElementRef} key={device.title}> <Device device={device}></Device> </div>
-        } else {
-          return <div ref={lastDeviceElementRef} key={device.title}> <Device device={device} ></Device> </div>
-        }
-      })}
+  const onDragOver = (event) => {
+    event.preventDefault()
+  }
 
-      <div>{loading && 'Loading...'}</div>
-      <div>{error && 'Error'}</div>
-    </div>
+  const onDrop = (event) => {
+    // get the name of the device we are dragging into the gear bag
+    let deviceName = event.dataTransfer.getData("newDevice");
+
+    // get the actual device by iterating through the full device array and finding the
+    // device with the matching name
+    let newDevices = devices.filter((potDevice) => {
+      if (potDevice.title === deviceName) {
+        return potDevice
+      }
+    });
+
+    // setting the gearbag to include the previous devices and the new (dragged) device
+    // making the array a Set, then returning the Set spread out in an array to eliminate duplicates
+    setGearBag(prevGearBag => {
+      return [...new Set([...prevGearBag, ...newDevices])]
+    })
+  }
+
+  //TODO: make it so that devices can be dragged out of the bag
+  //TODO: save gear bag between refreshes
+  //TODO: add a search bar?
+  //TODO: fix visuals
+      // do something with default images?
+      // make device names more readable
+      // make device summaries fit in box (and more readable)
+      // make gear bag look better...however I decide to do that lol
+      // add design to rest of website (background, tab icon, menu nav bar, etc)
+
+  return (
+    <>
+      {/* The main div that holds all of the loaded devices */}
+      <div style={devicesFlex}>
+        {devices.map((device, index) => {
+          // if we are returning the last device of the load, we are marking it as the last one so that once
+          // it is on the screen, we load more devices
+          if (devices.length === index + 1) {
+            return <div ref={lastDeviceElementRef} key={device.title}> <Device device={device}></Device> </div>
+          } else {
+            return <div key={device.title}> <Device device={device} ></Device> </div>
+          }
+        })}
+
+        {/* simple divs to signify if we are getting more devices, or if we encountered an error */}
+        <div>{loading && 'Loading...'}</div>
+        <div>{error && 'Error'}</div>
+      </div>
+
+      {/* Here is the div for the gear bag at the bottom of the screen */}
+      <div style={gearBagStyle} onDrop={(event) => onDrop(event)} onDragOver={(event) => onDragOver(event)}>
+        <h3 style={{paddingLeft: 10}}>Your Gear:</h3>
+        <div style={gearBagFlex}>
+          {gearBag.map((device) => {
+            return <div key={device.title}> <Device device={device}></Device> </div>
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/Device.js
+++ src/components/Device.js
@@ -3,19 +3,21 @@ import React, { useState } from 'react'
 const Device = ( { device } ) => {
   // the device component will display the images of each device as well as the title
   // then, when the mouse hovers over it will display the summary
-  //TODO finally, it needs to be able to be dragged into the grab bag
-  //TODO remove device when dragged into bag
-  //TODO replace device when dragged out of bag
+  //TODO signify when device is in the gear bag
+  //TODO remove signal when device dragged out of bag
 
   const [nameTextStyle, setNameStyle] = useState({display: 'block'})
   const [summaryTextStyle, setSummaryStyle] = useState({display: 'none'})
+  const [inGearBag, setInGearBag] = useState(false)
   
+  // deviceStyle is the CSS to style the div holding the image and text for each device
   const deviceStyle = {
     width: 150,
     height: 100,
     margin: 6,
     padding: 8,
     color: "white",
+    textAlign: "center",
     border: "1px solid green",
     padding: 8,
     backgroundImage: `url(${device.image.thumbnail}`,
@@ -24,12 +26,21 @@ const Device = ( { device } ) => {
     backgroundSize: "cover",
   }
 
+  const onDragStart = (event, deviceName) => {
+    event.dataTransfer.setData("newDevice", deviceName)
+  }
+
+  // return one div with the background image as the device image, and two <p>s
+  // that contain the name and summary info. It shows the name by default, then
+  // hovering over the div shows the summary info
   return (
-    <div style={deviceStyle}
+    <div style={deviceStyle} draggable='true' onDragStart={(event, deviceName) => onDragStart(event, `${device.title}`)}
+      // when mouse hovers over the device, hide the title and show the description
       onMouseEnter = {e => {
         setNameStyle({display: 'none'})
         setSummaryStyle({display: 'block'})
       }}
+      // when mouse stops hovering over the device, hide the summary and show the title
       onMouseLeave = {e => {
         setNameStyle({display: 'block'})
         setSummaryStyle({display: 'none'})
